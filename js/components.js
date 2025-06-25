d2492bd8bd960f18da57e83b17b8f68823ceff92
manu <manuellatourf@gmail.com>
2025-06-25 13:10:29 +0200

    añadir meowrhino en el about y modificar textos y li last child

--- js/components.js
+++ js/components.js
@@ -9,30 +9,33 @@ export function injectComponents() {
     `;
   document.body.appendChild(nav);
 
-  // Popup bio
-  const popup = document.createElement("div");
-  popup.id = "andrea-popup";
-  popup.innerHTML = `
-        <button class="close-btn" id="close-andrea">cerrar</button>
-        <div class="content">
-            <p>Graduada en Comunicación Audiovisual por la Universidad de Granada en 2019, su práctica fotográfica se
-                articula entre la moda, el diario personal y la fotografía robada, tensionando los límites entre lo
-                documental y lo construido. </p>
-            <p>​​Es cofundadora de la editorial de autoedición <a
-                href="https://quiennocorrevuela.bigcartel.com/" target="_blank">Quien no corre, vuela</a>, desde donde ha publicado
-                <i>archivo en pixel</i> y <i>no time left for square</i>, investigando nuevas formas de narrar, publicar y distribuir
-                fotografía contemporánea desde los márgenes.</p>
-            <p>Su trabajo se construye desde la serialidad y la repetición: las imágenes no funcionan de forma aislada,
-                sino que se organizan como fragmentos de una narrativa abierta. Dispara desde el impulso y el error, con
-                una cámara que no busca certezas ni discursos cerrados, sino que reacciona ante lo que hiere, incomoda o
-                emociona. Una mirada crítica, permeable y radicalmente encarnada en lo cotidiano.</p>
-            <div class="footer">
-                <a href="mailto:carillagonzalezandrea@gmail.com">carillagonzalezandrea@gmail.com</a>
-                <a href="https://www.instagram.com/andreacarilla/" target="_blank">@andreacarilla</a>
-            </div>
+// Popup bio
+const popup = document.createElement("div");
+popup.id = "andrea-popup";
+popup.innerHTML = `
+    <button class="close-btn" id="close-andrea">cerrar</button>
+    <div class="content">
+        <p>Graduada en Comunicación Audiovisual por la Universidad de Granada en 2019, su práctica fotográfica se
+            articula entre la moda, el diario personal y la fotografía robada, tensionando los límites entre lo
+            documental y lo construido. </p>
+        <p>​​Es cofundadora de la editorial de autoedición <a
+            href="https://quiennocorrevuela.bigcartel.com/" target="_blank">Quien no corre, vuela</a>, desde donde ha publicado
+            <i>archivo en pixel</i> y <i>no time left for square</i>, investigando nuevas formas de narrar, publicar y distribuir
+            fotografía contemporánea desde los márgenes.</p>
+        <p>Su trabajo se construye desde la serialidad y la repetición: las imágenes no funcionan de forma aislada,
+            sino que se organizan como fragmentos de una narrativa abierta. Dispara desde el impulso y el error, con
+            una cámara que no busca certezas ni discursos cerrados, sino que reacciona ante lo que hiere, incomoda o
+            emociona. Una mirada crítica, permeable y radicalmente encarnada en lo cotidiano.</p>
+        <div class="footer">
+            <a href="mailto:carillagonzalezandrea@gmail.com">carillagonzalezandrea@gmail.com</a>
+            <a href="https://www.instagram.com/andreacarilla/" target="_blank">@andreacarilla</a>
+            <span style="position:absolute; bottom:1rem;">
+                web hecha en el marco de <a href="https://meowrhino.github.io/becasDigMeow/" target="_blank">becas digitalización meowrhino</a>
+            </span>
         </div>
-    `;
-  document.body.appendChild(popup);
+    </div>
+`;
+document.body.appendChild(popup);
 
   // Botón "home"
   if (document.body.dataset.pageType !== "home") {
